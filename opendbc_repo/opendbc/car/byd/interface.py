09fbb62baa6c0bc1d9552f015a803028e139aea0
Comma Device <device@comma.ai>
2025-09-28 06:26:21 +0000

    更改接口参数

--- opendbc_repo/opendbc/car/byd/interface.py
+++ opendbc_repo/opendbc/car/byd/interface.py
@@ -59,7 +59,8 @@ class CarInterface(CarInterfaceBase):
             return self.torque_from_lateral_accel_linear
 
     @staticmethod
-    def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams: # type: ignore
+    #def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams: # type: ignore
+    def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, is_release, docs) -> structs.CarParams:
         ret.brand = "byd"
         ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.byd)]
 
